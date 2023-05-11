4ea205e128c3b223ef96b6ce92142c1c89ff9125
hanbee17 <hanbee.cho@sjsu.edu>
2023-05-11 01:05:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = choosePic;
 
-var myPix = ['p1.jpg', 'p2.jpg', 'p3.jpg', 'p5.jpg', 'p6.jpg', 'p7.jpg', 'p8.jpg'];
+var myPix = ['p1.jpg', 'p2.jpg', 'p3.jpg', 'p5.jpg', 'p6.jpg', 'p7.jpg', 'p8.jpg', 'p10.jpg'];
 
 function choosePic() {
      var randomNum = Math.floor(Math.random() * myPix.length);

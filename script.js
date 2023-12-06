c29ffdfdba1c19c358b6e306b573869a687f59c8
hanbee17 <hanbee.cho@sjsu.edu>
2023-12-05 23:44:12 -0800

    1

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onload = choosePic;
 
-var myPix = ['p1.jpg', 'p2.jpg', 'p3.jpg', 'p5.jpg', 'p6.jpg', 'p7.jpg', 'p8.jpg', 'p10.jpg'];
+var myPix = ['p1.jpg', 'p2.jpg', 'p3.jpg', 'p5.jpg', 'p6.jpg', 'p7.jpg', 'p8.jpg', 'p10.jpg','p13.png'];
 
 function choosePic() {
      var randomNum = Math.floor(Math.random() * myPix.length);
-     document.getElementById("myPicture").src = "./photos_assets/" + myPix[randomNum];
+     document.getElementById("myPicture").src = "photo_assets/" + myPix[randomNum];
    }
